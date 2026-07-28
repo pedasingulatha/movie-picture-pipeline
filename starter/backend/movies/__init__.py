e648d803b3217e23e1a1ceeda71b79049379de3d
Pedasingu Durga Latha <dlktk534@gmail.com>
2026-07-28 12:56:24 +0000

    Fix uwsgi configuration and deploy backend successfully

--- starter/backend/movies/__init__.py
+++ starter/backend/movies/__init__.py
@@ -1,5 +1,11 @@
-from flask import Blueprint
+import os
+from flask import Flask
+from flask_cors import CORS
 from .resources import Movies
+from flask import Blueprint
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 movies_api = Blueprint("movies_api", __name__)
 movies = Movies.as_view("movies")
@@ -7,3 +13,8 @@ movies = Movies.as_view("movies")
 movies_api.add_url_rule("/movies", strict_slashes=False, defaults={"movie_id": None}, view_func=movies, methods=["GET"])
 movies_api.add_url_rule("/movies", view_func=movies, methods=["POST"])
 movies_api.add_url_rule("/movies/<int:movie_id>", view_func=movies, methods=["GET", "PUT", "DELETE"])
+
+app.register_blueprint(movies_api)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("FLASK_RUN_PORT", 5000)))
