497be16aa0c9ad90e164dc6e0f74985a3a44cae4
Pedasingu Durga Latha <dlktk534@gmail.com>
2026-07-28 14:10:23 +0000

    Fix flake8 linter issues in app.py

--- starter/backend/app.py
+++ starter/backend/app.py
@@ -1,6 +1,8 @@
-import sys
 import os
+import sys
+
+from movies import app as application  # noqa: E402, F401
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from movies import app
+app = application
